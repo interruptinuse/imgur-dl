342f4f2b2374a53b998a5ba88a3db9362a453d87
inty <interruptinuse@gmail.com>
2023-02-21 18:54:30 +0000

    setup.py: Add install_requires

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='imgurdl',
     packages=find_packages(),
+    install_requires=["tqdm", "parsel", "esprima", "termcolor", "requests"],
     entry_points = {
         'console_scripts': [
             'imgur-dl = imgurdl.__main__:main'
